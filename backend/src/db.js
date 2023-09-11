19a40c70f45dcf7768a3cf2434c9461a92815912
NAlonsoBulacio <nicoalonso99.na@gmail.com>
2023-09-11 10:28:37 -0300

    pg con ssl

--- backend/src/db.js
+++ backend/src/db.js
@@ -7,6 +7,10 @@ const {
 } = process.env;
 
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: true, // Habilitar SSL/TLS
+  },
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
