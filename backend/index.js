d95b99540ae7b086f29ac8e80fd24dd9d556e526
NAlonsoBulacio <nicoalonso99.na@gmail.com>
2023-09-12 11:57:43 -0300

    limpiar bd

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ const { conn } = require("./src/db.js");
 require("dotenv").config();
 const { PORT } = process.env;
 // Syncing all the models at once.
-conn.sync({ alter: true }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(PORT, () => {
     console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
   });
