4050af9c3c1205b05adddc201489379fe34e15ef
nicoalonsoo <clubparistuc@gmail.com>
2023-09-19 13:00:33 -0300

    db_port

--- backend/src/db.js
+++ backend/src/db.js
@@ -3,10 +3,10 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
+  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
   dialect: 'postgres',
   // dialectOptions: {
   //   ssl: true, // Habilitar SSL/TLS
