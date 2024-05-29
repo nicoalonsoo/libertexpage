7461edc6adc2992032b32f82c902750187a2c4ad
NAlonsoBulacio <nicoalonso99.na@gmail.com>
2024-05-29 19:58:42 -0300

    no pixel directly

--- client/src/index.js
+++ client/src/index.js
@@ -7,11 +7,9 @@ import { ThemeProvider } from "@material-tailwind/react";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import store from "./redux/store";
-import MetaPixel from "./utils/MetaPixel/MetaPixel";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <MetaPixel />
     <Provider store={store}>
       <BrowserRouter>
         <ThemeProvider>
