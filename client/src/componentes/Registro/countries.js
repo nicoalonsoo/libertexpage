ff5b072c649a71ef45508177c5cdb224d1324b45
NAlonsoBulacio <nicoalonso99.na@gmail.com>
2024-06-14 15:06:02 -0300

    countries

--- client/src/componentes/Registro/countries.js
+++ client/src/componentes/Registro/countries.js
@@ -1,28 +1,13 @@
 const countries = [
-    { code: '+1767', suf: [''], name: 'Dominica', flag: 'https://flagcdn.com/dm.svg', continent: 'North America' },
-    { code: '+595', suf: [''], name: 'Paraguay', flag: 'https://flagcdn.com/py.svg', continent: 'South America' },
-    { code: '+504', suf: [''], name: 'Honduras', flag: 'https://flagcdn.com/hn.svg', continent: 'North America' },
-    { code: '+1246', suf: [''], name: 'Barbados', flag: 'https://flagcdn.com/bb.svg', continent: 'North America' },
     { code: '+54', suf: [''], name: 'Argentina', flag: 'https://flagcdn.com/ar.svg', continent: 'South America' },
-    { code: '+52', suf: [''], name: 'Mexico', flag: 'https://flagcdn.com/mx.svg', continent: 'North America' },
-    { code: '+502', suf: [''], name: 'Guatemala', flag: 'https://flagcdn.com/gt.svg', continent: 'North America' },
-    { code: '+297', suf: [''], name: 'Aruba', flag: 'https://flagcdn.com/aw.svg', continent: 'North America' },
-    { code: '+1', suf: ['', ''], name: 'Puerto Rico', flag: 'https://flagcdn.com/pr.svg', continent: 'North America' },
-    { code: '+503', suf: [''], name: 'El Salvador', flag: 'https://flagcdn.com/sv.svg', continent: 'North America' },
-    { code: '+53', suf: [''], name: 'Cuba', flag: 'https://flagcdn.com/cu.svg', continent: 'North America' },
     { code: '+591', suf: [''], name: 'Bolivia', flag: 'https://flagcdn.com/bo.svg', continent: 'South America' },
-    { code: '+51', suf: [''], name: 'Peru', flag: 'https://flagcdn.com/pe.svg', continent: 'South America' },
-    { code: '+506', suf: [''], name: 'Costa Rica', flag: 'https://flagcdn.com/cr.svg', continent: 'North America' },
-    { code: '+57', suf: [''], name: 'Colombia', flag: 'https://flagcdn.com/co.svg', continent: 'South America' },
-    { code: '+507', suf: [''], name: 'Panama', flag: 'https://flagcdn.com/pa.svg', continent: 'North America' },
-    { code: '+1242', suf: [''], name: 'Bahamas', flag: 'https://flagcdn.com/bs.svg', continent: 'North America' },
-    { code: '+509', suf: [''], name: 'Haiti', flag: 'https://flagcdn.com/ht.svg', continent: 'North America' },
-    { code: '+598', suf: [''], name: 'Uruguay', flag: 'https://flagcdn.com/uy.svg', continent: 'South America' },
-    { code: '+505', suf: [''], name: 'Nicaragua', flag: 'https://flagcdn.com/ni.svg', continent: 'North America' },
     { code: '+56', suf: [''], name: 'Chile', flag: 'https://flagcdn.com/cl.svg', continent: 'South America' },
-    { code: '+1473', suf: [''], name: 'Grenada', flag: 'https://flagcdn.com/gd.svg', continent: 'North America' },
-    { code: '+1441', suf: [''], name: 'Bermuda', flag: 'https://flagcdn.com/bm.svg', continent: 'North America' },
-    { code: '+593', suf: [''], name: 'Ecuador', flag: 'https://flagcdn.com/ec.svg', continent: 'South America' }
+    { code: '+57', suf: [''], name: 'Colombia', flag: 'https://flagcdn.com/co.svg', continent: 'South America' },
+    { code: '+593', suf: [''], name: 'Ecuador', flag: 'https://flagcdn.com/ec.svg', continent: 'South America' },
+    { code: '+52', suf: [''], name: 'Mexico', flag: 'https://flagcdn.com/mx.svg', continent: 'North America' },
+    { code: '+595', suf: [''], name: 'Paraguay', flag: 'https://flagcdn.com/py.svg', continent: 'South America' },
+    { code: '+51', suf: [''], name: 'Peru', flag: 'https://flagcdn.com/pe.svg', continent: 'South America' },
+    { code: '+598', suf: [''], name: 'Uruguay', flag: 'https://flagcdn.com/uy.svg', continent: 'South America' }
 ];
 
 export default countries;
